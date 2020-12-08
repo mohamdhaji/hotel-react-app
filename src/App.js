06c0f532e87206938cc88f36c62a0d15609606e0
mohamd haji <mohamdhaji0007@gmail.com>
2020-12-08 15:23:01 +0200

    editing router basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Routes from "./routes";
 import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/hotel-react-app/">
       <Routes />
     </BrowserRouter>
   );
